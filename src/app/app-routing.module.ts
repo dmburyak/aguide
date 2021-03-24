92faa63199f541ec35a1fa92ec532f041193d5e4
Dmitri <d.buryak@gmail.com>
2021-03-24 16:11:16 +0200

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AngularPageComponent} from './angular-page/angular-page.component';
+import {RxjsPageComponent} from './rxjs-page/rxjs-page.component';
+import {MaterialPageComponent} from './material-page/material-page.component';
+import {PageNotFoundComponent} from './shared/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/angular', pathMatch: 'full'},
+  {path: 'angular', component: AngularPageComponent},
+  {path: 'rxjs', component: RxjsPageComponent},
+  {path: 'angular-material', component: MaterialPageComponent},
+  {path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MainLayoutComponent} from './shared/main-layout/main-layout.component';
+import {AngularPageComponent} from './angular-page/angular-page.component';
+import {RxjsPageComponent} from './rxjs-page/rxjs-page.component';
+import {MaterialPageComponent} from './material-page/material-page.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    AngularPageComponent,
+    RxjsPageComponent,
+    MaterialPageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
